98821d4572ab224297d64b6fc2b3003d3a53ce16
tarequi <t.anshasi99@gmail.com>
2025-03-16 23:57:39 +0300

    clean up

--- front-end/src/app/socket.service.ts
+++ front-end/src/app/socket.service.ts
@@ -11,7 +11,7 @@ export class AppSocket extends Socket {
       options: {
         path: environment.socket_path,
         autoConnect: false,
-        auth: { authorization: 'Barer ' + token },
+        query: { authorization: 'Barer ' + token },
       },
     });
   }
