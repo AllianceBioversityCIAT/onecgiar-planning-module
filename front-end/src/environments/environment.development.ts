94e69329feb8dab85279a457471e18a014789bb3
moayadnajd <moayad.najd@gmail.com>
2025-10-27 21:16:47 +0300

    development

--- front-end/src/environments/environment.development.ts
+++ front-end/src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
     socket_path:"/socket.io",
-    socket_url:"http://localhost:3000",
-    api_url:'http://localhost:3000',
+    socket_url:"/api",
+    api_url:'/api',
     aws_cognito_link:'https://ost-toc.auth.us-east-1.amazoncognito.com',
     aws_cognito_client_id:'7j40ekbpnbq4aegcsgjmm66c90',
     aws_cognito_client_redirect_uri:'https://localhost:4200/auth'
