a951394ed2e2d5c6c0467fff1f27507a329e9c6d
moayadnajd <moayad.najd@gmail.com>
2025-10-28 18:05:29 +0300

    team member tab (empty field) #267

--- front-end/src/app/team-members/granted-access-pipe.pipe.ts
+++ front-end/src/app/team-members/granted-access-pipe.pipe.ts
@@ -5,8 +5,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class GrantedAccessPipePipe implements PipeTransform {
 
-  transform(data: any[]): string {
-
+  transform(data: any[], role: string = ''): string {
+     let newRoles = ['MELIA Focal Point', 'Financial Focal Point'];
+    if (!Array.isArray(data) || data.length === 0) {
+      if(newRoles.includes(role)) {
+        return role;
+      } else {
+        return 'Full access';
+      }
+    }
     return data.map((d: any) => d.name).join(', ');
   }
 }
