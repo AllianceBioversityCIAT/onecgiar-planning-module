0d3e7ab32133103403c02a2aa61f4384c6394400
tarequi <t.anshasi99@gmail.com>
2025-11-03 18:11:00 +0300

    https://codeobia.atlassian.net/browse/PLA-569

--- front-end/src/app/share/editor/editor.component.ts
+++ front-end/src/app/share/editor/editor.component.ts
@@ -76,29 +76,6 @@ export class EditorComponent implements OnInit {
       defaultFontName: '',
 
       defaultFontSize: '2',
-      // fonts: [
-      //   { class: 'arial', name: 'Arial' },
-      //   { class: 'times-new-roman', name: 'Times New Roman' },
-      //   { class: 'calibri', name: 'Calibri' },
-      //   { class: 'comic-sans-ms', name: 'Comic Sans MS' },
-      // ],
-      // customClasses: [
-      //   {
-      //     name: 'quote',
-      //     class: 'quote',
-      //   },
-      //   {
-      //     name: 'redText',
-      //     class: 'redText',
-      //   },
-      //   {
-      //     name: 'titleText',
-      //     class: 'titleText',
-      //     tag: 'h1',
-      //   },
-      // ],
-      // uploadUrl: 'v1/image',
-      // upload: (file: File) => { ... }
       uploadWithCredentials: false,
       sanitize: true,
       toolbarPosition: 'top',
@@ -111,36 +88,17 @@ export class EditorComponent implements OnInit {
     else
       return [
         [
-          // 'undo',
-          // 'redo',
-          // 'bold',
-          // 'italic',
-          // 'underline',
           'strikeThrough',
           'subscript',
           'superscript',
-          // 'justifyLeft',
-          // 'justifyCenter',
-          // 'justifyRight',
-          // 'justifyFull',
           'indent',
           'outdent',
-          // 'insertUnorderedList',
-          // 'insertOrderedList',
-          // 'heading',
           'fontName',
         ],
         [
-          // 'fontSize',
-          // 'textColor',
-          // 'backgroundColor',
           'customClasses',
-          // 'link',
-          // 'unlink',
           'insertImage',
           'insertVideo',
-          // 'insertHorizontalRule',
-          // 'removeFormat',
           'toggleEditorMode',
         ],
       ];
