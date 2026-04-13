b09f98ca17d6d6bd15b91b141e7475ed9511b484
moayadnajd <moayad.najd@gmail.com>
2026-04-13 16:16:55 +0300

    Update production environment to use /api and relative socket URL

--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
-    buildVersion: 'dev',
+    buildVersion: '',
     socket_path:"/socket.io",
-    socket_url:"https://api.planning.cgiar.org",
-    api_url:'https://api.planning.cgiar.org',
+    socket_url:"https://planning.cgiar.org",
+    api_url:'/api',
     aws_cognito_link:'https://osttoc.auth.us-east-1.amazoncognito.com',
     aws_cognito_client_id:'1qpvt3bdablluaf8aphovpglna',
     aws_cognito_client_redirect_uri:'https://planning.cgiar.org/auth',
